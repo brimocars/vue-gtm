f7bcb88eab0b09be6b1effba1d64ba1488aee863
Brian Mocarski <brian.mocarski.contractor@fujifilm.com>
2024-07-22 14:50:34 -0400

    delete index.js and .npmignore

--- index.js
+++
@@ -1,7 +0,0 @@
-/* eslint-disable */
-import * as vueGtmModule from "./dist";
-
-export default vueGtmModule.default;
-export const createGtm = vueGtmModule.createGtm;
-export const useGtm = vueGtmModule.useGtm;
-export const install = vueGtmModule.install;

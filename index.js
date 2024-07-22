73c48e5eda37cee12c08b23ef7be27677aeef13a
Brian Mocarski <brian.mocarski.contractor@fujifilm.com>
2024-07-22 13:09:23 -0400

    add index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+/* eslint-disable */
+import * as vueGtmModule from "./dist";
+
+export default vueGtmModule.default;
+export const createGtm = vueGtmModule.createGtm;
+export const useGtm = vueGtmModule.useGtm;
+export const install = vueGtmModule.install;
